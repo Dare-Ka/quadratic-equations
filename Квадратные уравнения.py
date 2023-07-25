188b2e498ec94a371bc76048bdecbacf4835ce48
Maksim Kozhin <a.zurnaaaa@gmail.com>
2023-07-25 17:43:33 +0300

    quadratic-equations 2.5

--- Квадратные уравнения.py
+++ Квадратные уравнения.py
@@ -1,18 +1,23 @@
-def discriminant(a, b, c):
-  return b**2 - 4*a*c
-def quadratic_equation(a, b, c):
-  if discriminant(a, b, c) == 0:
-    x = -b/(2*a)
-    print(f'x = {x} -----> x = {round(x, 2)}')
-    return x
-  else:
-    x1 = (-b + discriminant(a, b, c)**0.5)/(2*a)
-    x2 = (-b - discriminant(a, b, c)**0.5)/(2*a)
-    print(f'discriminant = {discriminant(a, b, c)}')
-    print(f'x1 = {x1} -----> x1 = {round(x1.real, 2) + round(x1.imag, 2)*1j}')
-    print(f'x2 = {x2} -----> x2 = {round(x2.real, 2) + round(x2.imag, 2)*1j}')
-    return
-a= float(input('Введите число a: '))
-b = float(input('Введите число b: '))
-c = float(input('Введите число c: '))
-quadratic_equation(a, b, c)
+class Coefficient:
+    def __init__(self, list):
+        self.a = list[0]
+        self.b = list[1]
+        self.c = list[2]
+
+    def discriminant(self, a, b, c):
+        return self.b ** 2 - 4 * self.a * self.c
+
+    def solution(self):
+        if self.discriminant(self.a, self.b, self.c) == 0:
+            x = -self.b / (2 * self.a)
+            return f'x = {x} -----> x = {round(x, 2)}'
+        else:
+            x1 = (-self.b + self.discriminant(self.a, self.b, self.c) ** 0.5) / (2 * self.a)
+            x2 = (-self.b - self.discriminant(self.a, self.b, self.c) ** 0.5) / (2 * self.a)
+            return f'discriminant = {self.discriminant(self.a, self.b, self.c)}\nx1 = {x1} -----> x1 = {round(x1.real, 2) + round(x1.imag, 2) * 1j}\nx2 = {x2} -----> x2 = {round(x2.real, 2) + round(x2.imag, 2) * 1j}'
+
+while True:
+    coefficients = Coefficient([int(num) for num in input('Введите коэффициенты: ').split()])
+    print(coefficients.solution())
+    if input('Повторить программу? (Y/N) ').strip().upper() != 'Y':
+        break
